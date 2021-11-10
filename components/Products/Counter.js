835f0cdf810ee9e4f122af7c51f41bee3bfec6a1
Diego Plata <diegooplata@gmail.com>
2021-11-10 00:10:12 -0500

    Improve counter

--- components/Products/Counter.js
+++ components/Products/Counter.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text } from 'react-native'
 import intervalToDuration from 'date-fns/intervalToDuration'
+import { endOfDay } from 'date-fns'
 
 const Counter = ({ endDate, screen }) => {
   const [counter, setCounter] = useState({
@@ -12,26 +13,31 @@ const Counter = ({ endDate, screen }) => {
   })
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      let interval
-      const now = new Date()
+    const today = new Date()
+    if (endDate.toDate() > today) {
+      const timer = setInterval(() => {
+        let interval
+        const now = new Date()
 
-      interval = intervalToDuration({
-        start: now,
-        end: endDate.toDate(),
-      })
-      const { months, days, hours, minutes, seconds } = interval
+        interval = intervalToDuration({
+          start: now,
+          end: endDate.toDate(),
+        })
+        const { months, days, hours, minutes, seconds } = interval
 
-      setCounter({ months, days, hours, minutes, seconds })
-    }, 1000)
-    return () => clearInterval(timer)
+        setCounter({ months, days, hours, minutes, seconds })
+      }, 1000)
+      return () => clearInterval(timer)
+    }
   })
 
   return (
     <Text style={screen === 'products' ? styles.timerProducts : styles.timer}>
       {counter?.months
         ? `${counter.months}m ${counter.days}d ${counter.hours}h ${counter.minutes}m ${counter.seconds}s`
-        : ` ${counter.days}d ${counter.hours}h ${counter.minutes}m ${counter.seconds}s`}
+        : ` ${counter.days || 0}d ${counter.hours || 0}h ${counter.minutes || 0}m ${
+            counter.seconds || 0
+          }s`}
     </Text>
   )
 }
